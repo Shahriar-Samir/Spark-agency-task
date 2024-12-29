fb91d178b7eaca01a397ade37e0344093d0d7c0e
Shahriar-Samir <shabusiness035@gmail.com>
2024-12-29 18:56:01 +0600

    add: footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import "swiper/css";
 import Header from "./components/Header";
 import OVERONS from "./components/OVERONS";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <main className="mx-auto">
       <Header />
       <OVERONS />
+      <Footer />
     </main>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,34 @@
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="mt-52">
+      <div className="flex justify-between items-end px-5">
+        <h5 className="text-6xl">
+          Overtuigd van de nummer 1 in Noord Holland ?
+        </h5>
+        <h1 className="text-[250px] leading-[200px]  font-nobile font-bold">
+          GMK
+        </h1>
+      </div>
+      <div className="flex justify-between px-5 mt-10 mb-10 font-actor text-lg">
+        <p>
+          GMK Dakonderhoud B.V. <br />
+          Leidsekade 57 1016 CX Amsterdam
+        </p>
+        <p>
+          info@dakonderhoudgmk.nl <br />
+          (020) 26 12 257
+        </p>
+        <p>
+          Privacy verklaring <br />
+          Algemene Voorwaarden
+        </p>
+        <p>
+          (C) MMXXIV GMK Dakonderhoud B.V. <br />
+          Concept by Nakatori Amsterdam
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
